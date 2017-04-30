d7cb5a02811256f911a56420a8dae7ea3db8df71
Johann Garces <hello@johannmg.com>
2017-04-30 19:18:23 -0400

    working on team models

--- SlackStatus/Models/Team.swift
+++ SlackStatus/Models/Team.swift
@@ -3,12 +3,26 @@
 
 import Foundation
 
-class Team {
+class Team: NSCoding {
     let id: String
     let name: String
+    
+    static let teamIdKey = "TeamIdentifierString"
+    static let teamNameKey = "TeamNameString"
     
     init (id: String, name: String){
         self.id = id
         self.name = name
     }
+    
+    required init(coder decoder: NSCoder){
+        self.id = decoder.decodeObject(forKey: Team.teamIdKey) as! String
+        self.name = decoder.decodeObject(forKey: Team.teamNameKey) as! String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(id, forKey: Team.teamIdKey)
+        aCoder.encode(name, forKey: Team.teamNameKey)
+    }
 }
+
--- SlackStatus/Services/TeamManager.swift
+++ SlackStatus/Services/TeamManager.swift
@@ -13,7 +13,7 @@ import Foundation
 
 /*
  Managers mutiple login for teams
-    - Team name to display
+    - Update team name to display
     - Team last active (for app restart)
     - All logged-in team ids
     - Login Saving
@@ -21,7 +21,31 @@ import Foundation
     - Get API key from keychain from team id
  
  */
+
+struct TeamKeys{
+    static let allTeamsKey = "teams.allteams"
+}
+
 class TeamManager {
+    
+    static func loggedInWithTeam(_ team: Team, AndApiTokenForTeam: String){
+        
+    }
+    
+    static func addTeam(_ team: Team) {
+        var allTeams = TeamManager.getAllTeams() ?? [Team]()
+        allTeams.append(team)
+        saveUpdateTeamsList(allTeams)
+    }
+    
+    static func saveUpdateTeamsList(_ team: [Team]){
+        UserDefaults.standard.set(team, forKey: TeamKeys.allTeamsKey)
+    }
+    
+    static func getAllTeams() -> [Team]? {
+        return UserDefaults.standard.array(forKey: TeamKeys.allTeamsKey) as? [Team]
+    }
+    
     
     
 }
