504b392f3b8b4098d3af970420846d17e77f162b
Johann Garces <hello@johannmg.com>
2017-04-30 22:12:02 -0400

    add constants. move things around

---
+++ SlackStatus/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+
+import UIKit
+
+struct kAppColors {
+    static let brightBlue = UIColor(red: 0.275, green: 0.804, blue: 0.769, alpha: 1.00)
+    static let deepBlue = UIColor(red: 0.333, green: 0.384, blue: 0.443, alpha: 1.00)
+    static let deepRed = UIColor(red: 1.000, green: 0.416, blue: 0.404, alpha: 1.00)
+    
+    static let darkOrange = UIColor(red: 0.961, green: 0.400, blue: 0.149, alpha: 1.00)
+    static let brightOrange = UIColor(red: 0.949, green: 0.553, blue: 0.235, alpha: 1.00)
+    
+    static let topGradBlue = UIColor(red: 0.333, green: 0.384, blue: 0.443, alpha: 1.00)
+    static let bottomGrabBlue = UIColor(red: 0.106, green: 0.204, blue: 0.314, alpha: 1.00)
+    
+    
+}
---
+++ SlackStatus/Views/LoadingView.swift
@@ -0,0 +1,38 @@
+//
+//  LoadingView.swift
+//  SlackStatus
+//
+//  Created by Johann Garces on 4/30/17.
+//  Copyright © 2017 johannmg. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit(){
+        
+        
+        
+    }
+
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
